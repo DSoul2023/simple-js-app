e805eb37fc8ef7c5f144c4a751de69035bbbd6ba
DSoul2023 <dantestaton2012@gmail.com>
2023-08-08 05:26:01 -0400

    Updated Pokedex to display each Pokemon name according to its type, and indicate larger sized Pokemon.
    Used a conditional for loop to display each Pokemon to the DOM.
    Added color and stylized font to each Pokemon name according to its type.
    Added statement to indicate larger Pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 let pokemonList = [
-    {name: 'Bulbasour', height: 7, type: ['grass','poison']},
-    {name: 'Charmander', height: 6, type: ['fire']},
-    {name: 'Squirtle', height: 5, type: ['water']}
+    {name: 'Bulbasour', height: 0.7, type: ['grass','poison']},
+
+    {name: 'Charmander', height: 0.6, type: ['fire']},
+
+    {name: 'Squirtle', height: 0.5, type: ['water']}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+   if (pokemonList[i].height >= 0.7 ) {
+        document.write ("<p>" + "<span class = grass-name>" + pokemonList[i].name + "</span>" + " (height: " + pokemonList[i].height + "m). " + "<span class= exclaim>" + " Wow, that's big!" + "</span>" + "</p>");
+    } else if (pokemonList[i].height < 0.7 && pokemonList[i].height > 0.5 ) {
+        document.write ("<p>" + "<span class = fire-name>" + pokemonList[i].name + "</span>" + " (height: " + pokemonList[i].height + "m). " + "</p>");
+    } else {
+        document.write ("<p>" + "<span class = water-name>" + pokemonList[i].name + "</span>" + " (height: " + pokemonList[i].height + "m). " + "</p>");
+    }
+}
