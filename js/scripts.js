88aa63fc4ddc9f9e7c74cf5aa573f1223a855f1a
DSoul2023 <dantestaton2012@gmail.com>
2023-08-16 06:48:51 -0400

    Code conversion
    Refactored Pokedex code from a for loop to a forEach() loop.

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ let pokemonList = [
     {name: 'Squirtle', height: 0.5, type: ['water']}
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
+/*for (let i = 0; i < pokemonList.length; i++) {
    if (pokemonList[i].height >= 0.7 ) {
         document.write ("<p>" + "<span class = grass-name>" + pokemonList[i].name + "</span>" + " (height: " + pokemonList[i].height + "m). " + "<span class= exclaim>" + " Wow, that's big!" + "</span>" + "</p>");
     } else if (pokemonList[i].height < 0.7 && pokemonList[i].height > 0.5 ) {
@@ -14,4 +14,13 @@ for (let i = 0; i < pokemonList.length; i++) {
     } else {
         document.write ("<p>" + "<span class = water-name>" + pokemonList[i].name + "</span>" + " (height: " + pokemonList[i].height + "m). " + "</p>");
     }
-}
+}*/
+pokemonList.forEach(function(pokemon) {
+    if (pokemon.height >= 0.7 ) {
+        document.write ("<p>" + "<span class = grass-name>" + pokemon.name + "</span>" + " (height: " + pokemon.height + "m). " + "<span class= exclaim>" + " Wow, that's big!" + "</span>" + "</p>");
+    } else if (pokemon.height < 0.7 && pokemon.height > 0.5 ) {
+        document.write ("<p>" + "<span class = fire-name>" + pokemon.name + "</span>" + " (height: " + pokemon.height + "m). " + "</p>");
+    } else {
+        document.write ("<p>" + "<span class = water-name>" + pokemon.name + "</span>" + " (height: " + pokemon.height + "m). " + "</p>");
+    }
+});
