9f419d287de4f2e0c26c857ddd7942fc048b3a59
DSoul2023 <dantestaton2012@gmail.com>
2023-08-25 20:10:55 -0400

    added Pokemon description buttons to code
    Added function to create a button element for each Pokemon in the Pokemon array with an event listener that pinpoints which button has been pressed and revised code for a more professional layout.

--- js/scripts.js
+++ js/scripts.js
@@ -6,30 +6,46 @@ let pokemonRepository = (function () { //Globl variable declaration containing b
         {name: 'Charmander', height: 0.6, type: ['Fire']},
     
         {name: 'Squirtle', height: 0.5, type: ['Water']}
-    ]
+    ];
+    function add (pokemon) {//function for adding content to array outside of IIFE
+        if (typeof pokemon ==='object' && 'name' in pokemon && 'height' in pokemon && 'type' in pokemon) {
+            pokemonList.push(pokemon);
+        } else {
+            console.log('invalid entry');
+        }
+    }
         function getAll () {//function for calling contents of array outside of IIFE
             return pokemonList; 
         }
-        function add (item) {//function for adding content to array outside of IIFE
-        pokemonList.push(item);
+        
+        function addListItem(pokemon){
+            let pokemonList = document.querySelector('.pokemon-list'); //connects to ul element class
+            let listItem = document.createElement('li'); //creates an li element under ul element
+            let button = document.createElement('button'); //creates a button
+            button.innerText = pokemon.name; //names button after pokemon in the array
+            button.classList.add('button-class'); //adds class to button element for styling purposes
+            listItem.appendChild(button); //attaches button to li
+            pokemonList.appendChild(listItem); //attaches li to ul
+            button.addEventListener('click', function(event) {//Notify console that a particular button has been preessed
+                console.log(event); 
+            })
+        }
+        function showDetails(pokemon) {
+            console.log(pokemon)
         }
     return {
       getAll: getAll, //key-value pairs
-      add: add
+      add: add,
+      addListItem: addListItem
     };
   })();//end of IIFE.
-  console.log(pokemonRepository.getAll());
+  //adds following pokemon to the repository
   pokemonRepository.add ({name:'Vulpix', height:2, type: ['Fire']});
   pokemonRepository.add ({name:'Dragonite', height:7.03, type: ['Dragon','Flying']});
   pokemonRepository.add ({name:'Raichu', height:2.07, type: ['Electric']});
+
   console.log(pokemonRepository.getAll());
 
  pokemonRepository.getAll().forEach(function(pokemon) { //used .getAll() to call contents of pokemon repository outside of IIFE
-        if (pokemon.height >= 7 ) {
-            document.write ("<p>" + "<span class = name>" + pokemon.name + "</span>" + " (height: " + pokemon.height + "m). " + "<span class= exclaim>" + " Wow, that's big!" + "</span>" + "</p>");
-        } else if (pokemon.height < 0.7 && pokemon.height > 0.5 ) {
-            document.write ("<p>" + "<span class = name>" + pokemon.name + "</span>" + " (height: " + pokemon.height + "m). " + "</p>");
-        } else {
-            document.write ("<p>" + "<span class = name>" + pokemon.name + "</span>" + " (height: " + pokemon.height + "m). " + "</p>");
-        };
+      pokemonRepository.addListItem(pokemon); //calls addListItem functions contenets
     })
